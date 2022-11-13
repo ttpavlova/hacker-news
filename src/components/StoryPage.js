de8fdce63c0430da0c4bcc961de10d0f58232cf6
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-11-14 00:30:10 +0300

    Add 'Back to news' button

--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 import Comments from "./Comments";
 import Story from "./Story";
 
-function StoryPage({ id }) {
+function StoryPage(props) {
+
+    const id = props.id;
+
+    function backToNews() {
+        props.history.goBack();
+    }
 
     return (
         <div>
             <h3>Requested topic ID: {id}</h3>
+            <button onClick={() => backToNews()}>Back to news</button>
             <Story id={id} />
             <Comments id={id} />
         </div>
     );
 }
 
-export default StoryPage;
+export default withRouter(StoryPage);
